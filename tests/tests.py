6166b3946e040a16c167418886544b48e933e698
kash8580 <kashish.singh1101@gmail.com>
2024-11-19 10:35:09 +0530

    commit2

--- tests/tests.py
+++ tests/tests.py
@@ -3,11 +3,9 @@ from operations.operations import rev,palin
 class testing(unittest.TestCase):
     def test_rev(self):
         self.assertEqual(rev("myst"),"tsym")
-
-
     
     def test_palin(self):
-        
+
         self.assertTrue(palin("naman"),True)
 
 if __name__=="__main__":
